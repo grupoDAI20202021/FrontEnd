6b3c17aa93f0bd671d395cdf5dc48eb365d0cca6
grupoDAI20202021 <grupoDAI20202021@gmail.com>
2021-03-09 17:07:12 +0000

    massas 09 03 2021 17 07

--- desktop/Frontend/adminkit-master/static/js/adminCamara.js
+++ desktop/Frontend/adminkit-master/static/js/adminCamara.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-
+  let cent= new Array("helo","byek");
     $('#Table-town-hall').DataTable({
+      autoFill:true,
       "language": {
-        "lengthMenu": "Mostrar _MENU_ centralist por página",
+        "lengthMenu": "Mostrar _MENU_ câmara por página",
         "zeroRecords": "Nada encontrado",
         "info": "Mostrar página _PAGE_ de _PAGES_",
-        "infoEmpty": "Nenhhum centralista",
+        "infoEmpty": "Nenhhuma câmara",
         "infoFiltered": "(Filtrado _MAX_ centralista)",
         "search": "Procurar:",
         "paginate": {
@@ -24,4 +25,26 @@ $(document).ready(function() {
          "searchable": false
        }]*/
     });
+    let tabela = document.getElementById("Table-town-hall");
+      let linhas = tabela.getElementsByTagName("tr");
+
+      for (let i = 0; i < linhas.length; i++) {
+        var linha = linhas[i];
+        linha.addEventListener("click", function() {
+          //Adicionar ao atual
+          selLinha(this, false); //Selecione apenas um
+          //selLinha(this, true); //Selecione quantos quiser
+        });
+      }
+
+      function selLinha(linha, multiplos) {
+        if (!multiplos) {
+          let linhas = linha.parentElement.getElementsByTagName("tr");
+          for (let i = 0; i < linhas.length; i++) {
+            var linha_ = linhas[i];
+            linha_.classList.remove("selecionado");
+          }
+        }
+        linha.classList.toggle("selecionado");
+      }
   });
