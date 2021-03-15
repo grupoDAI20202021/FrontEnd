f276d30621b77375d6e80a806ebce63de4709665
grupoDAI20202021 <grupoDAI20202021@gmail.com>
2021-03-15 22:05:30 +0000

    eqgqeg

--- desktop/Frontend/adminkit-master/static/js/instituicaoCalendar.js
+++ desktop/Frontend/adminkit-master/static/js/instituicaoCalendar.js
@@ -1,9 +1,9 @@
-import Calendar from 'tui-calendar'; /* ES6 */
-import "tui-calendar/dist/tui-calendar.css";
+var Calendar = require('tui-calendar'); /* CommonJS */
+require("tui-calendar/dist/tui-calendar.css");
 
 // If you use the default popups, use this.
-import 'tui-date-picker/dist/tui-date-picker.css';
-import 'tui-time-picker/dist/tui-time-picker.css';
+require("tui-date-picker/dist/tui-date-picker.css");
+require("tui-time-picker/dist/tui-time-picker.css");
 $(document).ready(function() {
     
 var calendar = new Calendar('#calendar', {
@@ -37,30 +37,4 @@ var calendar = new Calendar('#calendar', {
         isReadOnly: true    // schedule is read-only
     }
 ]);
-});
-
-calendar.updateSchedule(schedule.id, schedule.calendarId, {
-    start: startTime,
-    end: endTime
-});
-
-calendar.deleteSchedule(schedule.id, schedule.calendarId);
-
-calendar.on('beforeCreateSchedule', function(event) {
-    var startTime = event.start;
-    var endTime = event.end;
-    var isAllDay = event.isAllDay;
-    var guide = event.guide;
-    var triggerEventName = event.triggerEventName;
-    var schedule;
-
-    if (triggerEventName === 'click') {
-        // open writing simple schedule popup
-        //schedule = {...};
-    } else if (triggerEventName === 'dblclick') {
-        // open writing detail schedule popup
-        //schedule = {...};
-    }
-
-    calendar.createSchedules([schedule]);
 });
