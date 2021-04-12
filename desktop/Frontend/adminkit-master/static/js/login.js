b76d7dc36a8acde8065efc0af9ee9334afe9fe81
grupoDAI20202021 <grupoDAI20202021@gmail.com>
2021-04-12 12:28:14 +0100

    massas 12 04

--- desktop/Frontend/adminkit-master/static/js/login.js
+++ desktop/Frontend/adminkit-master/static/js/login.js
@@ -5,20 +5,78 @@ function signUp() {
 function signIn() {
   document.getElementById("container").classList.remove("right-panel-active");
 }
-function login() {
-  const email = document.getElementById("email_login").value;
-  const password = document.getElementById("password_login").value;
-  fetch("http://127.0.0.1:8080/api/", {
-    method: "POST",
-    body: { email: email, password: password },
-  })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+window.onload = async function () {
+  localStorage.clear();
+  const botaoLogin = document.getElementById("botaoLogin");
+
+
+  botaoLogin.addEventListener("click", entrar);
+
+
+  async function entrar() {
+      event.preventDefault();
+      var data = {};
+
+      data.email = document.getElementById("email_login").value.trim();
+      data.password = document.getElementById("password_login").value.trim();
+
+      fetch('http://127.0.0.1:8080/api/auth/signin', {
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+          },
+          mode: 'cors',
+          method: 'POST',
+          body: JSON.stringify(data),
+          credentials: 'include'
+      })
+          .then(function (response) {
+
+              console.log(response);
+              if (!response.ok) {
+                  throw new Error(response.statusText);
+              }
+              return response.json();
+          })
+          .catch(function (err) {
+              console.log(err);
+          })
+          .then(async function (result) {
+              if (result) {
+                  console.log(result);
+                  localStorage.setItem("userLogado", result.userId);
+                  localStorage.setItem("RoleLogado", result.role);
+
+                  Swal.fire("Sucesso!",
+                      "Autenticado com sucesso!",
+                      "success")
+                      .then(() => {
+                        if (result.role !== "ROLE_INSTITUTION") {
+                          window.location.replace("../html/InstituicaoDashboard.html");
+                      } if (result.role !== "ROLE_ADMINISTRATOR") {
+                          window.location.replace("../html/AdminDashboard.html");
+                      }
+                      if (result.role !== "ROLE_TOWNHALL") {
+                        window.location.replace("../html/CamaraDashboard.html");
+                      }
+                  });
+              } else {
+                  Swal.fire(
+                      'Os dados que inseriu não estão corretos!',
+                      '',
+                      'warning'
+                  )
+                  console.log(result);
+
+              }
+
+          });
+
+  };
+
+
+
+};
 
 function register() {
   const name = document.getElementById("name_register").value;
@@ -37,4 +95,20 @@ function register() {
         console.log(error);
       });
   }
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
 }
