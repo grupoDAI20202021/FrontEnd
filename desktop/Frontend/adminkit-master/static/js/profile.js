1231b49e3fb98b4fed9c3085937591e1f1e2df22
Imperadeiro00 <duimpera4@gmail.com>
2021-05-06 23:38:12 +0100

    Duarte update

--- desktop/Frontend/adminkit-master/static/js/profile.js
+++ desktop/Frontend/adminkit-master/static/js/profile.js
@@ -1,7 +1,13 @@
 function changeEmail() {
   const email = document.getElementById("email_change").value;
-  fetch(
-    `http://127.0.0.1:8080/api/institutions/${localStorage.getItem(
+  let endpoint;
+  if (role === "ROLE_INSTITUTION") {
+    endpoint = "institution";
+  }
+  if (role === "ROLE_TOWNHALL") {
+    endpoint = "townhall";
+  }
+  fetch(`http://127.0.0.1:8080/api/${endpoint}/${localStorage.getItem(
       "userLogado"
     )}`,
     {
@@ -20,10 +26,16 @@ function changeEmail() {
 function changePassword() {
   const password = document.getElementById("password_change").value;
   const confirmPassword = document.getElementById("password_confirm").value;
-  fetch(
-    `http://127.0.0.1:8080/api/institutions/${localStorage.getItem(
-      "userLogado"
-    )}/password`,
+  let endpoint;
+  if (role === "ROLE_INSTITUTION") {
+    endpoint = "institution";
+  }
+  if (role === "ROLE_TOWNHALL") {
+    endpoint = "townhall";
+  }
+  fetch(`http://127.0.0.1:8080/api/${endpoint}/${localStorage.getItem(
+    "userLogado"
+  )}/password`,
     {
       method: "PUT",
       body: { password: password, confirmPassword: confirmPassword },
@@ -56,3 +68,24 @@ window.onload = function () {
     }
   });
 };
+
+window.onload = function () {
+  const role = localStorage.getItem("RoleLogado");
+  const user = localStorage.getItem("userLogado");
+  let endpoint;
+  if (role === "ROLE_INSTITUTION") {
+    endpoint = "institution";
+  }
+  if (role === "ROLE_TOWNHALL") {
+    endpoint = "townhall";
+  }
+  fetch(`http://127.0.0.1:8080/api/${endpoint}/${user}`).then((response) => {
+    if (role === "ROLE_INSTITUTION") {
+      document.getElementById("instit_nome").innerHTML = response.name;
+    }
+    if (role === "ROLE_TOWNHALL") {
+      document.getElementById("Camara_Admin_Nome").innerHTML = response.name;
+    }
+  });
+};
+
