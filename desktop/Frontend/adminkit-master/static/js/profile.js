7bf422c9153b20bcb0aad063cd3055dafcef70d2
Imperadeiro00 <duimpera4@gmail.com>
2021-05-11 23:38:00 +0100

    Duarte Update

--- desktop/Frontend/adminkit-master/static/js/profile.js
+++ desktop/Frontend/adminkit-master/static/js/profile.js
@@ -33,7 +33,6 @@ function changeEmail() {
 }
 
 function changePassword() {
-<<<<<<< HEAD
   const password = document.getElementById("password_change").value;
   const confirmPassword = document.getElementById("password_confirm").value;
   let endpoint;
@@ -46,16 +45,6 @@ function changePassword() {
   fetch(`http://127.0.0.1:8080/api/${endpoint}/${localStorage.getItem(
     "userLogado"
   )}/password`,
-=======
-  let data= {};
-  data.password = document.getElementById("newPass").value;
-   data.confirmPassword = document.getElementById("confirmPass").value;
-  data.oldPassword= "wrbsfd";
-  fetch(
-    `http://127.0.0.1:8080/api/institutions/${localStorage.getItem(
-      "userLogado"
-)}/password`,
->>>>>>> a0f9da041d74dfd174bd0bc337ef64ee12b50b2c
     {
       headers: {
         'Content-Type': 'application/json'
